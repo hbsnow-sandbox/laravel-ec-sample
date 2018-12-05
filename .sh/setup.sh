9cf54633d56afa7cad10c7379c77b0dbddb3202e
hbsnow <yuki.4uing@gmail.com>
2018-12-05 23:24:23 +0900

    refct: remame

--- .sh/setup.sh
+++ .sh/setup.sh
@@ -2,8 +2,8 @@ SCRIPT_DIR=$(cd $(dirname $0); pwd)
 
 cd $SCRIPT_DIR
 
-cp -f conf/.laradock-env ../laradock-ec-sample/.env
-cp -f conf/xdebug.ini ../laradock-ec-sample/php-fpm/xdebug.ini
-cp -f conf/php7.2.ini ../laradock-ec-sample/php-fpm/php7.2.ini
-cp -f conf/xdebug.ini ../laradock-ec-sample/workspace/xdebug.ini
-cp -f conf/.laravel-env ../src/.env
+cp -f conf-local/.laradock-env ../laradock-ec-sample/.env
+cp -f conf-local/xdebug.ini ../laradock-ec-sample/php-fpm/xdebug.ini
+cp -f conf-local/php7.2.ini ../laradock-ec-sample/php-fpm/php7.2.ini
+cp -f conf-local/xdebug.ini ../laradock-ec-sample/workspace/xdebug.ini
+cp -f conf-local/.laravel-env ../src/.env
