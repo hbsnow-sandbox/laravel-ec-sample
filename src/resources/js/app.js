5f5fe8995ccce7c4419ddb83e31f1879c804e977
hbsnow <yuki.4uing@gmail.com>
2018-12-26 00:27:33 +0900

    format

--- src/resources/js/app.js
+++ src/resources/js/app.js
@@ -1,40 +1,6 @@
 import '@babel/polyfill'
 import Vue from 'vue'
 
-const object1 = {
-  a: 1,
-  b: 2,
-  c: 3
-}
-
-const object2 = Object.assign({c: 4, d: 5}, object1)
-
-console.log(object2)
-
-/**
- * The following block of code may be used to automatically register your
- * Vue components. It will recursively scan this directory for the Vue
- * components and automatically register them with their "basename".
- *
- * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
-
-
-Vue.component('example-component', require('./components/ExampleComponent.vue'));
-
-// const files = require.context('./', true, /\.vue$/i)
-
-// files.keys().map(key => {
-//     return Vue.component(_.last(key.split('/')).split('.')[0], files(key))
-// })
-
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
-
-
 const app = new Vue({
-    el: '#app'
-});
-
-*/
+  el: '#app'
+})
